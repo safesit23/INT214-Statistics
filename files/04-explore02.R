3eb405adf44f5e0efbc8f48ee0fd9297d9a80391
jatawatsafe <jatawatsafe@hotmail.com>
2021-08-30 14:56:54 +0700

    [ADD] materials week 4

---
+++ files/04-explore02.R
@@ -0,0 +1,51 @@
+library(dplyr)
+glimpse(mtcars)
+
+# Import datasets from CSV
+Hotels <- read.csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/Hotels.csv")
+
+# Sample 1: head()
+head(Hotels,2)
+Hotels %>% head()
+
+# Sample 2: mean()
+mean(Hotels$RateperNight)
+Hotels$RateperNight %>% mean()
+
+str(Hotels)
+
+Hotels <- as_tibble(Hotels)
+str(Hotels)
+glimpse(Hotels)
+
+#Starwar
+glimpse(starwars)
+# MASS is DBL --> double
+
+starwars$height %>% 
+  mean(na.rm=T)
+
+# Topic 2 Data Manipulation
+# Select
+starwars %>% select(-height,-gender)
+# Filter
+starwars %>% filter(sex == "male")
+starwars %>% filter(!(sex == "male"))
+starwars %>% filter(sex == "male" | skin_color == "light")  # or
+starwars %>% filter(sex == "male",skin_color == "light")    # and
+#Summarise
+starwars %>% summarise(avg_height = mean(height, na.rm = TRUE),
+                       max_height = max(height, na.rm = TRUE))
+
+# Combining
+# Case 1 :correct
+starwars %>% select(name,height,mass, skin_color) %>% 
+  filter(skin_color=="light")
+#case 2: error
+starwars %>% select(name,height,mass) %>% 
+  filter(skin_color=="light")
+#case 3:
+starwars %>% filter(skin_color=="light") %>% select(name,height,mass)
+  
+
+
