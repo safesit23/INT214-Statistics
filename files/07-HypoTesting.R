fdb5365319fdad427dac5d430b2c1f6c5e2ac674
jatawatsafe <jatawatsafe@hotmail.com>
2021-11-05 13:44:43 +0700

    [UPDATE] HypoTesting

--- files/07-HypoTesting.R
+++ files/07-HypoTesting.R
@@ -1,12 +1,17 @@
 library(dplyr)
 library(readr)
 
+# Review
+sqrt(4)
+1:5
+
 # 1.) Sample and Population -------------
 # Sample Data
 sample(1:5, 3)
-sample(1:5, 3,replace = T)
+sample(1:5, 3,replace = T) # Sample with replacement
 
 # Sample n rows from a table
+View(mtcars)
 glimpse(mtcars)
 mtcars %>% sample_n(2)
 
@@ -18,18 +23,26 @@ population_1
 sample_1 <- sample(population_1 , 3)
 sample_1
 
-# 2.) Point Estimation
+# 2. Point Estimation ---------
 # Explore
 glimpse(starwars)
 count(starwars)
+
 # Mean
-mean(starwars$height, na.rm = T)
+#1.Finding average height of starwars characters.
+mean(starwars$height,na.rm = T)
+#2.Finding average height of starwars characters which sex is male.
 starwars %>% filter(sex=="male") %>% 
   summarise(avg_height=mean(height,na.rm = T)) %>% pull()
+
 # Proportion of male
 i <- starwars %>% filter(sex=="male") %>% count() %>% pull();i
 n <- starwars %>% count() %>% pull();n
 p <- i/n; p
+
+#pull example
+mtcars %>% pull(cyl)
+starwars %>% pull(height, name)
 
 # 3.) Normal Distribution
 
